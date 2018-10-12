7fa0a36a7128b84ec7d046ea932818309b8a1102
TSAI-WEN <TSAI-WEN@users.noreply.github.com>
2018-10-12 18:07:27 +0800

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+apple, orange, banana, lemon = argv
+
+print "The script is called:", apple
+print "Your first variable is:", orange
+print "Your second variable is:", banana
+print "Your third variable is:", lemon
